ba0b7f1433461550402b64d8350d37f77ef28283
Frank Simon <f.m.sim@t-online.de>
2018-07-14 01:18:28 +0200

    modified gulpfil.js, package.json, package-lock.json

--- wp-content/themes/wptorontoonline/gulpfile.js
+++ wp-content/themes/wptorontoonline/gulpfile.js
@@ -15,10 +15,10 @@ let reload = browserSync.reload;
 let onError = function(err) {
   console.log("An error occured: ", gutil.colors.magenta(err.message));
   gutil.beep();
-  this.emit("end");
+  this.emit("end")
 }
 
-gulp.task("sass", gulp.series(function() {
+gulp.task('sass', gulp.series(function() {
   return gulp.src("./sass/**/*.scss")
   .pipe(plumber({errorHandler: onError}))
   .pipe(sass())
@@ -27,39 +27,39 @@ gulp.task("sass", gulp.series(function() {
 
 }));
 
-gulp.task("js", gulp.series(function() {
-  return gulp.src(["./js/*.js"])
-  .pipe(concat("app.js"))
-  .pipe(rename({suffix: ".min"}))
+gulp.task('js', gulp.series(function() {
+  return gulp.src(['/js/*.js'])
+  .pipe(concat('app.js'))
+  .pipe(rename({suffix: '.min'}))
   .pipe(uglify())
-  .pipe(gulp.dest("./js"));
+  .pipe(gulp.dest('/js'))
 }));
 
-gulp.task("images", gulp.series(function() {
-  return gulp.src("images/src/*")
+gulp.task('images', gulp.series(function() {
+  return gulp.src('images/src/*')
   .pipe(plumber({errorHandler: onError}))
   .pipe(imagemin({optimizationLevel: 7, progessive: true}))
-  .pipe(gulp.dest("images/dest"));
+  .pipe(gulp.dest('images/dest'))
 
 }));
 
-gulp.task("watch", gulp.series(function() {
+gulp.task('watch', gulp.series(function() {
   browserSync.init({
-    files: ["./**/*.php"],
-    proxy: "http://localhost/wptorontoonline/"
-  });
+    files: ['./**/*.php'],
+    proxy: 'http://localhost/wptorontoonline/'
+  })
   gulp.series(function() {
-    "./sass/**/*.scss", ["sass", reload]
-  });
+    './sass/**/*.scss', ['sass', reload]
+  })
   gulp.series(function() {
-    "./js/*.js", ["js", reload]
-  });
+    './js/*.js', ['js', reload]
+  })
   gulp.series(function() {
-    "./images/src/*", ["images", reload]
-  });
+    './images/src/*', ['images', reload]
+  })
 }));
 
 // default task
-gulp.task("default", gulp.series(gulp.parallel("sass", "js", "images", "watch"), function() {
-  
+gulp.task('default', gulp.series(gulp.parallel('sass', 'js', 'images', 'watch'), function() {
+
 }));
