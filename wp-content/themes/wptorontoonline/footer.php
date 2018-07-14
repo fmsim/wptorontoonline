a5282db74da164d1ab1f46deb2e015bac0e61d3b
Frank Simon <f.m.sim@t-online.de>
2018-07-14 06:22:44 +0200

    removed gulp, added styles and navigation

--- wp-content/themes/wptorontoonline/footer.php
+++ wp-content/themes/wptorontoonline/footer.php
@@ -1,13 +1,15 @@
       </div> <!-- .container .content -->
     </div> <!-- #page -->
     <footer class="site-footer" role="contentinfo">
-      <nav id="footer-navigation" class="footer-navigation" role="navigation">
-        <?php
-          wp_nav_menu([
-            'theme_location' => 'main-menu'
-          ]);
-        ?>
-      </nav>
+      <div class="container">
+        <nav id="footer-navigation" class="footer-navigation" role="navigation">
+          <?php
+            wp_nav_menu([
+              'theme_location' => 'main-menu'
+            ]);
+          ?>
+        </nav>
+      </div> <!-- .container -->
       <hr>
       <div class="copyright">
         <p>Toronto Online <?php echo date('Y') ?></p>
@@ -16,6 +18,6 @@
     <!-- scripts -->
     <!--<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>-->
     <!-- <script type="text/javascript" src=".js"></script> -->
+    <?php wp_footer(); ?>
   </body>
-  <?php wp_footer(); ?>
 </html>
